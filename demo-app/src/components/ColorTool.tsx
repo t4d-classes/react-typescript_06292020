80b48894ee419c6cbc7525bb11f619a90d1357ea
Eric Greene <eric@t4d.io>
2020-06-29 07:52:39 -0400

    add exercise 4

--- demo-app/src/components/ColorTool.tsx
+++ demo-app/src/components/ColorTool.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
+import React, { FC } from 'react';
 
 import { Color } from '../models/Color';
 
-export const ColorTool = () => {
+export interface ColorToolProps {
+  colors: Color[];
+}
 
-  const colors: Color[] = [
-    { id: 1, name: 'blue', hexcode: '#0000FF' },
-    { id: 2, name: 'black', hexcode: '#000000' },
-    { id: 3, name: 'green', hexcode: '#00FF00' },
-    { id: 4, name: 'pink', hexcode: '#FFC0CB' },
-  ];
+export const ColorTool: FC<ColorToolProps> = (props) => {
 
-  // const colorListItems = [];
-
-  // for (let x=0; x<colors.length; x++) {
-  //   colorListItems.push(
-  //     <li key={colors[x].id}>
-  //       {colors[x].name}
-  //     </li>);
-  // }
-
-  const colorListItems = colors.map(c => 
+  const colorListItems = props.colors.map(c => 
     <li key={c.id}>
       {c.name}
     </li>
--- demo-app/src/index.tsx
+++ demo-app/src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Color } from './models/Color';
+
 import { ColorTool } from './components/ColorTool';
 import { CarTool } from './components/CarTool';
 
+const colorList: Color[] = [
+  { id: 1, name: 'blue', hexcode: '#0000FF' },
+  { id: 2, name: 'black', hexcode: '#000000' },
+  { id: 3, name: 'green', hexcode: '#00FF00' },
+  { id: 4, name: 'pink', hexcode: '#FFC0CB' },
+];
+
 ReactDOM.render(
   <>
-    <ColorTool />
+    <ColorTool colors={colorList} />
     <CarTool />
   </>,
   document.querySelector('#root'),
