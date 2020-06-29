044bbbf015afaf5188ef4e01f5ec9c244a7763a1
Eric Greene <eric@t4d.io>
2020-06-29 05:40:08 -0400

    add exercise 2

---
+++ demo-app/src/components/ColorTool.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const ColorTool = () => {
+  return (
+    <>
+      <header>
+        <h1>Color Tool</h1>
+      </header>
+      <ul>
+        <li>blue</li>
+        <li>black</li>
+        <li>green</li>
+        <li>pink</li>
+      </ul>
+    </>
+  );
+};
--- demo-app/src/index.tsx
+++ demo-app/src/index.tsx
@@ -2,34 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { HelloWorld } from './components/HelloWorld';
+import { CarTool } from './components/CarTool';
 
 ReactDOM.render(
-  // React.createElement(HelloWorld),
-  <HelloWorld />,
+  <>
+    <HelloWorld />
+    <CarTool />
+  </>,
   document.querySelector('#root'),
 );
-
-
-
-// // function declaration
-// function HelloWorld() {
-
-// }
-
-// HelloWorld();
-
-// // function expression
-// const HelloWorld2 = function() {
-
-// };
-
-// HelloWorld2();
-
-// // arrow function
-// const HelloWorld3 = () => {
-
-// };
-
-// HelloWorld3();
-
-
