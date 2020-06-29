3ec9827c080a1f41f7197b37c1c9c1c10415c7ba
Eric Greene <eric@t4d.io>
2020-06-29 05:08:34 -0400

    add exercise 1

--- demo-app/src/index.tsx
+++ demo-app/src/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-const HelloWorld = () => {
-
-  return React.createElement('h1', null, 'Hello, World!');
-
-};
+import { HelloWorld } from './components/HelloWorld';
 
 ReactDOM.render(
-  React.createElement(HelloWorld),
+  // React.createElement(HelloWorld),
+  <HelloWorld />,
   document.querySelector('#root'),
 );
 
